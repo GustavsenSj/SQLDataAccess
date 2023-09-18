b7b98a13307ab587484c20e22b273be86c3e2f45
Sjur G <sjur.gustavsen@no.experis.com>
2023-09-18 09:22:42 +0200

    Db connection

---
+++ SQLDataAccess/DB/DatabaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SQLDataAccess.DB;
+
+public class DatabaseConnection : IDisposable
+{
+    private readonly SqlConnection _connection;
+
+    public DatabaseConnection(string connectionString)
+    {
+        _connection = new SqlConnection(connectionString);
+    }
+
+    public SqlConnection GetConnection()
+    {
+        if (_connection.State != ConnectionState.Open)
+        {
+            _connection.Open();
+        }
+
+        return _connection;
+    }
+
+    public void Dispose()
+    {
+        if (_connection.State != ConnectionState.Closed)
+        {
+            _connection.Close();
+        }
+        _connection.Dispose();
+    }
+}
--- SQLDataAccess/Program.cs
+++ SQLDataAccess/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using SQLDataAccess.DB;
+using SQLDataAccess.Models;
+using SQLDataAccess.Repositories;
+using SQLDataAccess.Service;
 
-Console.WriteLine("Hello, World!");
+DatabaseConnection dbConnection = new DatabaseConnection(GetConnectionString());
+string GetConnectionString()
+{
+    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+    {
+        DataSource = "N-NO-01-03-2688",
+        InitialCatalog = "Chinook",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true
+    };
+    return builder.ConnectionString;
+}
